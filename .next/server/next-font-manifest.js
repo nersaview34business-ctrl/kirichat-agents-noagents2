0f7992ac4f4a8c3539c129dbaa72d13bb1fb55fe
nersaview34business-ctrl <nersaview34business@gmail.com>
2025-10-27 11:10:34 +0700

    Added build-manifest.json

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/a22b1dc24103c00d-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
